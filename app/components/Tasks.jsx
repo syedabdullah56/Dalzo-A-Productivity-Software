e5e808d77a25d3eca6def84336ef43d45886ca83
syedabdullah56 <syedabdullah8750@gmail.com>
2025-04-17 21:51:30 +0500

    Working On Functionalitties in /tasks page

--- app/components/Tasks.jsx
+++ app/components/Tasks.jsx
@@ -8,6 +8,26 @@ const Tasks = () => {
     const [todos, setTodos] = useState([]);
     const [refresh, setRefresh] = useState(false); // toggle to refresh on CRUD
 
+    const handleCreateTodo = async (data) => {
+      // calling API to create todo
+      await fetch("/api/todos", {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+    
+      setRefresh(!refresh); // to refresh todo list
+    };
+
+    const handleDeleteTodo = async (id) => {
+      await fetch(`/api/todos/${id}`, { method: "DELETE" });
+      setRefresh(!refresh);
+    };
+    
+    const handleEditTodo = (todo) => {
+      setSelectedTodo(todo); // open form in edit mode
+    };
+    
+
     //Fetch Todos From Backend
     useEffect(() => {
       const fetchTodos = async () => {
@@ -24,14 +44,16 @@ const Tasks = () => {
   return (
     <div className="space-y-6">
       {/* Todo Form For Creating New Todo */}
-      <TodoForm onRefresh={() => setRefresh(!refresh)}/>
-
+      <TodoForm onSubmit={handleCreateTodo} />
         {/* Todos List for each Todo Card */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
              {todos.map((todo) => (
-                 <TodoCard key={todo._id} todo={todo} onRefresh={() => setRefresh(!refresh)}/>
+                 <TodoCard key={todo._id} 
+                 onDelete={handleDeleteTodo}
+                 onEdit={handleEditTodo}
+                 todo={todo} onRefresh={() => setRefresh(!refresh)}/>
              ))}
-        </div>
+        </div> 
     </div>
   )
 }
--- app/components/TodoForm.jsx
+++ app/components/TodoForm.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-const TodoForm = () => {
+const TodoForm = ({ onSubmit, initialData = {} }) => {
+
+  const [title, setTitle] = useState("");
+  const [date,setDate]=useState("");
+  const [priority,setPriority]=useState("");
+
+  const handleSubmit=(e)=>{
+     e.preventDefault();
+     onSubmit({title,date,priority});
+     setTitle("");
+     setDate("");
+     setPriority("medium");
+
+  }
   return (
-    <div>TodoForm</div>
+    <form 
+    onSubmit={handleSubmit}
+    className='bg-white shadow-md rounded-xl p-6 w-full max-w-md mx-auto space-y-5'
+    >
+       <h2 className="text-xl font-semibold text-gray-700 text-center">Add / Edit Todo</h2>
+
+       <input
+       type='text'
+       placeholder='Enter title'
+       value={title}
+       onChange={(e)=>setTitle(e.target.value)}
+        className="text-black w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        required
+       />
+
+      <input
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        className="text-black w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        required
+      />
+
+      
+      <select
+        value={priority}
+        placeholder="Select Priority"
+        onChange={(e) => setPriority(e.target.value)}
+        className="text-black w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+      >
+        <option value="low">🔥 Low</option>
+        <option value="medium">⚡ Medium</option>
+        <option value="high">🚀 High</option>
+      </select>
+
+      <button
+        type="submit"
+        className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded-lg transition"
+      >
+        Submit
+      </button>
+
+
+
+    </form>
   )
 }
 
