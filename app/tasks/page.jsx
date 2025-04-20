da9bacc36f2cd2da29ccd66c1f214178b5190063
syedabdullah56 <syedabdullah8750@gmail.com>
2025-04-20 22:23:47 +0500

    Made a responsive container for my tasks

--- app/tasks/page.jsx
+++ app/tasks/page.jsx
@@ -1,15 +1,14 @@
 "use client"
-import Tasks from "../components/Tasks.jsx";
-
+import React from "react";
 
 const TasksPage = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Your Todos</h1>
-      
-      {/* task component */}
+    <div className="w-full h-screen flex justify-center  text-black">
+      <div className="w-[90vw] p-6 flex flex-row justify-center  bg-gray-100 rounded-xl shadow-lg md:w-[45vw]">
+        <h1 className="text-3xl font-bold mb-4">Your Tasks</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TasksPage;
